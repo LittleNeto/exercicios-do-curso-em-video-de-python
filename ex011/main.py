59508dbe6879cdc1cde71e92445ed6563d7c067a
José Agra Neto <agrajoseneto@gmail.com>
2025-04-14 17:35:53 -0300

    Create main.py

---
+++ ex011/main.py
@@ -0,0 +1,4 @@
+largura = float(input('largura da parede: '))
+altura = float(input('altura da parede: '))
+print(f'sua parede tem a dimensão de {largura}x{altura} e a sua área é de {largura * altura}m²')
+print(f'para pintar essa parede, você precisará de {largura * altura / 2:.1f}l de tinta')
