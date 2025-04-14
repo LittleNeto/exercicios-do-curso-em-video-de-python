32a35692c0a87602a07bc0f8609b26249db7df31
José Agra Neto <agrajoseneto@gmail.com>
2025-04-14 17:05:49 -0300

    Create main.py

---
+++ ex006/main.py
@@ -0,0 +1,4 @@
+num = int(input('digite um número: '))
+print(f'o dobro de {num} vale {num * 2}')
+print(f'o triplo de {num} vale {num * 3}')
+print(f'a raiz quadrada de {num} é igual a {(num ** (1 / 2)):.2f}')
