3d4e47cce2acc5120278d763b9cdf7c9aa12145e
José Agra Neto <agrajoseneto@gmail.com>
2025-04-15 15:08:42 -0300

    Update main.py

--- ex017/main.py
+++ ex017/main.py
@@ -1,4 +1,4 @@
 from math import hypot
 c1 = float(input('comprimento do cateto oposto: '))
 c2 = float(input('comprimento do cateto adjacente: '))
-print('a hipotenusa vai medir {:.2f}'.format(hypot(c1, c2)))
+print(f'a hipotenusa vai medir {hypot(c1, c2):.2f}')
